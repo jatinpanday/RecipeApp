9382535f6c81f071e604549cf85a0f94cc99a7d4
jatinpanday <pandayjatin98@gmail.com>
2023-06-06 00:47:36 +0530

    alignment fixes

--- src/App.js
+++ src/App.js
@@ -1,61 +1,62 @@
-import React,{useEffect, useState}from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Recipe from "./Recipe";
 
-const App = () =>{
+const App = () => {
 
   const APP_ID = "ff87099a";
   const APP_KEY = "f87d05b32f56f9e1070a96ff047cc25a";
-  
+
   const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] =useState("");
+  const [search, setSearch] = useState("");
   const [query, setQuery] = useState('chicken');
-  
+
   useEffect(() => {
     getRecipes();
   }, [query]);
 
-   const getRecipes = async () => {
-   const response = await fetch( `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
-   );
-   const data = await response.json();
-   setRecipes(data.hits);
-   console.log(data.hits);
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
   };
 
-  const updateSearch = e =>{
+  const updateSearch = e => {
     setSearch(e.target.value);
-    
+
   };
 
-  const getSearch = e =>{
+  const getSearch = e => {
     e.preventDefault();
     setQuery(search);
     setSearch('');
   }
 
-  return(
+  return (
     <div className="App">
       <form onSubmit={getSearch} className="search">
-        <input 
-        className="search-bar" 
-        type="text" value={search} 
-        onChange={updateSearch}
-          />
-      <button className="search-button" type="submit">
-        Search</button>
+        <input
+          className="search-bar"
+          type="text" value={search}
+          onChange={updateSearch}
+          placeholder='Search Recipe'
+        />
+        <button className="search-button" type="submit">
+          Search</button>
       </form>
       <div className="recipes">
-      {recipes.map(recipe => (
-        <Recipe 
-        key={recipe.recipe.label}
-        title={recipe.recipe.label}
-        calories={recipe.recipe.calories}
-        image={recipe.recipe.image}
-        ingredients={recipe.recipe.ingredients}
-        />
-      ))}
-    </div>
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={`${Math.ceil(recipe.recipe.calories)} Kcal`}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   )
 
--- src/Recipe.js
+++ src/Recipe.js
@@ -4,14 +4,13 @@ import style from "./recipe.module.css";
 const Recipe = ({ title, calories, image, ingredients }) => {
     return (
         <div className={style.recipe}>
-            <h1 className={style.title}>{title}</h1>
+            <h1 className={style.title}>{title} <small style={{ fontSize: "16px" }}>{calories}</small> </h1>
+            <img className={style.image} src={image} alt="" />
             <ol className={style.ingredients}>
                 {ingredients.map(ingredients => (
                     <li>{ingredients.text}</li>
                 ))}
             </ol>
-            <p className={style.calories}>{calories}</p>
-            <img className={style.image} src={image} alt="" />
         </div>
     );
 };
